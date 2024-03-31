9e89b3f69e104650fc0abd1a4afe297f6bf23453
Azamatjon <azamatergasev01@gmail.com>
2024-03-31 19:09:36 +0500

    from 1 to 6 tasks done

---
+++ main.js
@@ -0,0 +1,134 @@
+// 1 task start   
+
+// let list1 = [1,2,4] 
+// let list2 = [1,3,4] 
+// let result = [...list1 , ...list2] 
+// result.sort((a,b) => a - b) 
+// console.log(`Ikki array birlashmasi o'sish tartibida ${result}joylashtiril`) 
+
+
+// 1 task finished  
+
+
+
+// 2 task starts 
+
+//  1 usul 
+// let numbers = [4,5,6,7,0,1,2] 
+// let result = 0 
+
+// function findGeneralIndex (params) {
+
+//     for(let i = 0 ; i < numbers.length ; i++) {
+//         if (numbers[i] == params) {
+//              result = i
+//         }
+//     }
+// }
+// findGeneralIndex(0) 
+
+// console.log(`Kiritilgan son ${result} indexda joylashgan `);
+
+// 2 metodika stars 
+
+// function findIndex (params) {
+//     let arr = [4,5,6,7,0,1,2]
+
+//    let index = arr.indexOf(params)
+//    console.log(index);
+
+// }
+
+// findIndex(0)
+
+//  2 metodika finish 
+// 
+//  2 task finished 
+
+
+
+//  3 task starts  
+
+
+// function findValue (params) {
+//     let numbers = [5,7,7,8,8,10] 
+//     let result = []
+
+//     for(let i = 0 ; i < numbers.length ;i++ ) {
+//         if(numbers[i] === params) {
+//             result.push(numbers[i])
+
+
+//         }
+
+//     }
+//     if(result.length) {
+//         console.log(result)
+
+//     }else{
+//         console.log(`Berilgan array ichida hech narsa yuq`);
+//     }
+// }
+
+// findValue(8) 
+
+//  3 task finished  
+
+//  4 task starts 
+
+// function findDifference (params) {
+
+//     let arr =[1,2,3,4,5,99,6,7,8,9,100] 
+
+//     let max = arr[0]
+//     let min = arr[0] 
+
+//     for (let i = 1; i <= arr.length; i++) {
+//        if (max < arr[i]) {
+//         max = arr[i]  
+//        }
+
+//       if (min > arr[i]){
+//         min = arr[i]
+//       }
+//     }
+
+//     console.log(`params max sondan ${max - params} ga kichik`);
+//     console.log(`params min sondan ${params - min} ga katta`);
+// }
+
+// findDifference(4)
+
+
+
+
+// 4 task finished 
+
+
+// 5 task starts 
+
+
+// let objNumber = {number1 : 22, number2 : 2, number3: 3} 
+
+// let values = Object.values(objNumber) 
+
+// function addValues (params) {
+//         values.unshift(params)
+//         values.push(params)
+// }
+
+// addValues (99) 
+
+// console.log(`Berilgan objects ga ham boshidan ham oxiridan ham ${values} qo'shiladi`); 
+
+//  5 task finished  
+
+//  6 task starts 
+
+
+let objectsNumbers1 = {num1: 1 , num2: 22, num3: 33, num4: 44} 
+
+let objectsNumbers2 = {num1: 12, num2: 21, num3: 32, num4 :43} 
+
+
+
