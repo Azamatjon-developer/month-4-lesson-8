5813f0cb3ca0db6914d706752042a1725f1cc203
Azamatjon <azamatergasev01@gmail.com>
2024-03-31 23:32:49 +0500

    all js tasks done

--- main.js
+++ main.js
@@ -126,9 +126,86 @@
 //  6 task starts 
 
 
-let objectsNumbers1 = {num1: 1 , num2: 22, num3: 33, num4: 44} 
+// function findValue (params) {
+    
+//     let objOne = {numberOne: 10, numberTwo:20}
+//     let objTwo = {number3: 11, number4:21}
+//     let addObjects = {...objOne, ...objTwo}
 
-let objectsNumbers2 = {num1: 12, num2: 21, num3: 32, num4 :43} 
+//     let changeObjects = Object.values(addObjects)
+//     const max = Math.max(...changeObjects) 
+//     const min = Math.min(...changeObjects) 
+//     console.log(`Array ichidagi max son ${max} ga teng`);
+//     console.log(`Array ichidagi max son ${min} ga teng`);
+    
+    
+//     console.log(`params max sondan ${max - params} ga kichik`);
+//     console.log(`params min sondan ${params - min} ga katta`);
 
+// }
+
+// findValue(15)  
+
+// 6 task finished 
+
+
+// 7 task starts 
+
+
+// function numbers (params) {
+//     let obj1 = {num1:11 , num2: 12}
+//     let obj2 = {num3:13 , num4: 14}
+//     let obj3 = {num5:15 , num6: 16} 
+
+//     let addObjects = {...obj1, ...obj2, ...obj3}
+//     const changeObjects = Object.values(addObjects)
+//     let sum = 0 
+
+//     for(let i = 0 ; i < changeObjects.length; i++) {
+
+//        sum += changeObjects[i]
+//     }
+
+//     if (sum > params) {
+//         console.log(`array valuelari yigindisi  functiondagi qiymatdan ${sum - params} ga katta`); 
+
+//     }else{
+//         console.log(`functionda berilgan parametr qiymati array yigindisidan ${params - sum} ga katta`);
+//     }
+
+// }
+
+// numbers(1)
+
+// 7 task finished  
+
+
+//  8 task starts 
+
+
+// let arr1 = [1,3,2,0,4,5] 
+// let max = arr1[0]
+// let min = arr1[0] 
+
+// for (let i = 0 ; i < arr1.length; i++) {
+//     if (arr1[i] > max) {
+//         max = arr1[i]
+
+//     }
+//     if (arr1[i] < min) {
+//         min = arr1[i]
+//     }
+// }
+
+// const maxIndex = arr1.indexOf(max) 
+// const minIndex = arr1.indexOf(min)  
+
+// arr1[maxIndex] = min 
+// arr1[minIndex] = max
+
+
+// console.log(arr1);
+
+//  8 task finished 
 
 
